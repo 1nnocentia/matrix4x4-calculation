9c48dbc18898424682cb1924fc1e6d22ac4e3716
Innocentia <innocentiahan@gmail.com>
2024-12-27 09:46:47 +0800

    menyelesaikan fungsi invers matrix

--- invers_matrix.py
+++ invers_matrix.py
@@ -1,4 +1,7 @@
 from determinant_matrix import determinant
+from minor_matrix import minor
+from transpose_matrix import transpose
+from cofactor_matrix import cofactor
 def invers(m, c):
     det = determinant(m)
     if det == 0:
@@ -7,6 +10,8 @@ def invers(m, c):
         a, b = m[0]
         c, d = m[1]
         return [[d/det, -b/det], [-c/det, a/det]]
-
-                
-
+    else:
+        cofactors = cofactor(m,c)
+        adjoint = transpose(cofactors)
+        invers = [[adjoint[i][j] / det for j in range(c)] for i in range(c)]
+        return invers
